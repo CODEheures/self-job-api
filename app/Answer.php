716599ed2d8eb8ce613192002543da232a3eddb1
CODEheures <webmaster@codeheures.fr>
2018-02-11 02:51:32 +0100

    calc score type 3

--- app/Answer.php
+++ app/Answer.php
@@ -4,8 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\DB;
-use Sleimanx2\Plastic\Facades\Plastic;
+use ONGR\ElasticsearchDSL\Query\FullText\MatchQuery;
+use Sleimanx2\Plastic\DSL\SearchBuilder;
+use function Symfony\Component\Console\Tests\Command\createClosure;
 
 class Answer extends Model
 {
@@ -120,7 +121,7 @@ class Answer extends Model
 
             /**
              * Compare text with wanted and unwanted terms
-             * Score =
+             * Score = complex score by elastic score computation
              */
             case 3:
 
@@ -128,33 +129,145 @@ class Answer extends Model
                     return null;
                 }
 
-                $wantedSearch = trim(array_reduce($question->datas->wantedTerms,
-                    function ($carry, $item) {
-                        $carry .= (' ' . $item->label);
-                        return $carry;
-                    }
-                ));
-
-                $elasticScore3 = new Score3([
+                // The record of the answer
+                $score3 = Score3::create([
                     'documentIndex' => Score3::rootElasticIndex . $question->pref_language,
-                    'description' => $answer
+                    'description' => strtolower($answer)
                 ]);
-                $elasticScore3->save();
 
-                //sleep(5);
+                // The false record for calc the max possible elastic score of each wanted term
+                $wantedRecords = [];
+                foreach ($question->datas->wantedTerms as $term) {
+                    $wantedRecords[] = [
+                        'term' => strtolower($term->label),
+                        'record' => Score3::create([
+                            'documentIndex' => Score3::rootElasticIndex . $question->pref_language,
+                            'description' => strtolower($term->label)
+                        ])
+                    ];
+                }
+
+                // The false record for calc the max possible elastic score of each unwanted term
+                $unwantedRecords = [];
+                foreach ($question->datas->unwantedTerms as $term) {
+                    $unwantedRecords[] = [
+                        'term' => strtolower($term->label),
+                        'record' => Score3::create([
+                            'documentIndex' => Score3::rootElasticIndex . $question->pref_language,
+                            'description' => strtolower($term->label)
+                        ])
+                    ];
+                }
+
+                // Test if elasticsearch is ready for each new recording
                 $count = 0;
                 do {
-                    usleep(500000);
+                    $hits = 0;
+                    usleep(1000000);
                     $count++;
-                    $results = Score3::search()
+                    $isInElasticBase = Score3::search()
                         ->index(Score3::rootElasticIndex . $question->pref_language)
-                        ->must()->match('id', $elasticScore3->id)
-                        //->multiMatch(['description', 'description.stemmed'], $wantedSearch , ['fuzziness'=>'AUTO'])
+                        ->must()->match('id', $score3->id)
                         ->get();
-                } while ($results->totalHits() === 0 && $count < 10);
+                    $hits += $isInElasticBase->totalHits();
+
+                    foreach ($wantedRecords as $wantedRecord) {
+                        $isInElasticBase = Score3::search()
+                            ->index(Score3::rootElasticIndex . $question->pref_language)
+                            ->must()->match('id', $wantedRecord['record']->id)
+                            ->get();
+                        $hits += $isInElasticBase->totalHits();
+                    }
+
+                    foreach ($unwantedRecords as $unwantedRecord) {
+                        $isInElasticBase = Score3::search()
+                            ->index(Score3::rootElasticIndex . $question->pref_language)
+                            ->must()->match('id', $unwantedRecord['record']->id)
+                            ->get();
+                        $hits += $isInElasticBase->totalHits();
+                    }
+                } while ($hits < (1+count($wantedRecords)+count($unwantedRecords)) && $count < 10);
 
 
-                dd([$count, $results]);
+                // When elastic is ready to search in each record
+                $wantedScores = [];
+                $unwantedScores = [];
+                if ($hits === (1+count($wantedRecords)+count($unwantedRecords)) ) {
+                    foreach ($wantedRecords as $wantedRecord) {
+                        $wantedSearchMax = Score3::search()
+                            ->index(Score3::rootElasticIndex . $question->pref_language)
+                            ->must()->match('id', $wantedRecord['record']->id)
+                            ->multiMatch(['description', 'description.stemmed'], $wantedRecord['term'], ['fuzziness' => 'AUTO'])
+                            ->get();
+
+                        $wantedSearch = Score3::search()
+                            ->index(Score3::rootElasticIndex . $question->pref_language)
+                            ->must()->match('id', $score3->id)
+                            ->multiMatch(['description', 'description.stemmed'], $wantedRecord['term'], ['fuzziness' => 'AUTO'])
+                            ->get();
+
+                        $wantedScores[] = [
+                            'term' => $wantedRecord['term'],
+                            'max' => $wantedSearchMax->maxScore(),
+                            'score' => is_null($wantedSearch->maxScore()) ? 0 : $wantedSearch->maxScore()
+                        ];
+                    }
+
+                    foreach ($unwantedRecords as $unwantedRecord) {
+                        $unwantedSearchMax = Score3::search()
+                            ->index(Score3::rootElasticIndex . $question->pref_language)
+                            ->must()->match('id', $unwantedRecord['record']->id)
+                            ->multiMatch(['description', 'description.stemmed'], $unwantedRecord['term'], ['fuzziness' => 'AUTO'])
+                            ->get();
+
+                        $unwantedSearch = Score3::search()
+                            ->index(Score3::rootElasticIndex . $question->pref_language)
+                            ->must()->match('id', $score3->id)
+                            ->multiMatch(['description', 'description.stemmed'], $unwantedRecord['term'], ['fuzziness' => 'AUTO'])
+                            ->get();
+
+                        $unwantedScores[] = [
+                            'term' => $unwantedRecord['term'],
+                            'max' => $unwantedSearchMax->maxScore(),
+                            'score' => is_null($unwantedSearch->maxScore()) ? 0 : $unwantedSearch->maxScore()
+                        ];
+                    }
+                }
+
+
+                // Calc finals Scores
+                $max = 0;
+                $score = 0;
+                foreach ($wantedScores as $wantedScore) {
+                    $max += $wantedScore['max'];
+                    $score += $wantedScore['score'];
+                }
+
+                foreach ($unwantedScores as $unwantedScore) {
+                    $max += $unwantedScore['max'];
+                    $score += ($unwantedScore['max'] - $unwantedScore['score']);
+                }
+
+                $results = [
+                    'count' =>$count,
+                    'wantedScore' => $wantedScores,
+                    'unwantedScore' => $unwantedScores,
+                    'finalMax' => $max,
+                    'finalScore' => $score
+                ];
+
+
+
+                // Delete eslasticScore (in sql and elasticsearch base)
+                $score3->delete();
+                foreach ($wantedRecords as $wantedRecord) {
+                    $wantedRecord['record']->delete();
+                }
+                foreach ($unwantedRecords as $unwantedRecord) {
+                    $unwantedRecord['record']->delete();
+                }
+
+                return ['score' => $score, 'max' => $max];
                 break;
         }
 
