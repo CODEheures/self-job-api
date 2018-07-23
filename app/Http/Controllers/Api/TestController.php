b32042cb41e3961aa224018ba3221396e56e383a
CODEheures <webmaster@codeheures.fr>
2018-07-23 17:35:23 +0200

    set chmod 775 & chown www-data:

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -30,8 +30,8 @@ class TestController extends Controller
         // broadcast($newAdvertEvent);
 
         // Broadcast 11 answers to advert id 1
-         $newAnswerEvent = new NewAnswerEvent(1, 126);
-         broadcast($newAnswerEvent);
+        // $newAnswerEvent = new NewAnswerEvent(1, 126);
+        // broadcast($newAnswerEvent);
          return response('ok');
     }
 }
